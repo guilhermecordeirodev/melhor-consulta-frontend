7aa4f0ce852da758eb8355b9693fdd56d35f4c9c
Guilherme Cordeiro <guilherme.cordeiro@valecard.com.br>
2025-03-07 00:15:47 -0300

    adicionando favicon

--- src/shared/services/api/api.ts
+++ src/shared/services/api/api.ts
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: window.appConfig?.API_URL || "https://api.melhorconsulta.com",
+  // baseURL: window.appConfig?.API_URL || "https://api.melhorconsulta.com",
   //baseURL: "https://api.melhorconsulta.com"
-  // baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080",
 });
 
 export default api;
